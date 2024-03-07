ad90fb677431562a6bb935ac4fe34c3060510111
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-03-07 06:10:35 -0600

    Info

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,7 +4,14 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Daniel McGuire Corporation Docs</span>,
   project: {
-    link: 'https://danielmcguire.vercel.app/',
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  chat: {
+    link: 'https://discord.gg/VZRF3Sk5k8',
+  },
+  docsRepositoryBase: 'https://github.com/Daniel-McGuire-Corporation',
+  footer: {
+    text: 'GitHub',
   },
 }
 
