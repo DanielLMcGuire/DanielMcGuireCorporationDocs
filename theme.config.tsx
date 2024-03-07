b91a1535718f2ad12dff4a6d93bbd45338aca348
Daniel McGuire <146508360+DanielLMcGuire@users.noreply.github.com>
2024-03-07 06:12:44 -0600

    update

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,12 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Daniel McGuire Corporation Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/DanielLMcGuireCorporation/',
   },
   chat: {
     link: 'https://discord.gg/VZRF3Sk5k8',
   },
-  docsRepositoryBase: 'https://github.com/Daniel-McGuire-Corporation',
+  docsRepositoryBase: 'https://github.com/DanielLMcGuire/',
   footer: {
     text: 'GitHub',
   },
